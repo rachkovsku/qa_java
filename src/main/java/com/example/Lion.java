15725677623e63a9bc31d6c9867dff3af3cbaf98
Nikita Rachkov <infernalroze@yandex.ru>
2024-10-16 17:25:07 +0300

    Fixed Lion class

--- src/main/java/com/example/Lion.java
+++ src/main/java/com/example/Lion.java
@@ -8,6 +8,7 @@ public class Lion {
     private Feline feline;
 
     public Lion(String sex, Feline feline) throws Exception {
+        this.feline = feline;
         if ("Самец".equals(sex)) {
             hasMane = true;
         } else if ("Самка".equals(sex)) {
